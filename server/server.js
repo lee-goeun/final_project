849f92d100d3c55bda2f5b21d3948fca43977c2f
lee-goeun <koeun1120@duam.net>
2022-02-18 17:26:20 +0900

    test

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port =process.env.PORT || 3001;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use('/api', (req, res)=> res.json({username:'bryan'}));
+
+app.listen(port, ()=>{
+    console.log(`express is running on ${port}`);
+})
