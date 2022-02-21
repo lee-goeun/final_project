e5fa72f16a52b56475c7cefb04927fbfdd0f3a02
Nam Gideok <namgd1222@gmail.com>
2022-02-21 17:48:30 +0900

    post(게시물) 컨테이너 모달창 추가중

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -6,6 +6,9 @@ import {
   faComment,
   faEye,
   faEllipsisVertical,
+  faPen,
+  faXmark,
+  faX,
 } from '@fortawesome/free-solid-svg-icons';
 import {
   faHeart as borderHeart,
@@ -19,50 +22,59 @@ const PostContainer = () => {
 
   return (
     <>
-      <div className="post-container">
-        <div className="pc-left">
-          <img src={process.env.PUBLIC_URL + 'img/cat.png'} />
-          <div className="heart">200 ♥</div>
-        </div>
+      <div className="post-wrapper">
+        <div className="post-container">
+          <div className="pc-left">
+            <img src={process.env.PUBLIC_URL + 'img/cat.png'} />
+            <div className="heart">200 ♥</div>
+          </div>
 
-        <div className="pc-right">
-          <div className="pr01">
-            <img src={process.env.PUBLIC_URL + 'img/cam.jpg'} />
-          </div>
-          <div className="pr02">
-            <h2>ilovepet</h2>
-            <span
-              onClick={() => {
-                setIsFollow(!isFollow);
-              }}
-            >
-              {isFollow ? '팔로잉' : '팔로우'}
-            </span>
-          </div>
-          <div className="pr03">
-            <FontAwesomeIcon
-              icon={faEllipsisVertical}
-              id="dots-icon"
-              onClick={() => {
-                setShowPostMenu(!showPostMenu);
-              }}
-            />
-            {/* 수정/삭제 모달창 */}
-            {showPostMenu && (
-              <div className="menu-modal-container">
-                <p>수정하기</p>
-                <p>삭제하기</p>
-              </div>
-            )}
-          </div>
-          <div className="pr04">본문내용</div>
-          <div className="pr05">
-            <p>2022/02/20 14:15</p>
-          </div>
-          <div className="pr06">댓글창</div>
-          <div className="pr07">
-            <input type="text" placeholder="댓글 남기기" />
-            <button>ENTER</button>
+          <div className="pc-right">
+            <div className="pr01">
+              <img src={process.env.PUBLIC_URL + 'img/cam.jpg'} />
+            </div>
+            <div className="pr02">
+              <h2>ilovepet</h2>
+              <span
+                onClick={() => {
+                  setIsFollow(!isFollow);
+                }}
+              >
+                {isFollow ? '팔로잉' : '팔로우'}
+              </span>
+            </div>
+            <div className="pr03">
+              <FontAwesomeIcon
+                icon={faEllipsisVertical}
+                id="dots-icon"
+                onClick={() => {
+                  setShowPostMenu(!showPostMenu);
+                }}
+              />
+              {/* 수정/삭제 모달창 */}
+              {showPostMenu && (
+                <div className="menu-modal-container">
+                  <p>수정하기</p>
+                  <p>삭제하기</p>
+                </div>
+              )}
+              <FontAwesomeIcon icon={faX} id="modal-off-icon" title="창닫기" />
+            </div>
+            <div className="pr04">본문내용</div>
+            <div className="pr05">
+              <p>2022/02/20 14:15</p>
+            </div>
+            <div className="pr06">
+              <CommentContainer />
+              <CommentContainer />
+              <CommentContainer />
+              <CommentContainer />
+              <CommentContainer />
+            </div>
+            <div className="pr07">
+              <input type="text" placeholder="댓글 남기기" />
+              <button>ENTER</button>
+            </div>
           </div>
         </div>
       </div>
@@ -92,4 +104,32 @@ const MiniPostContainer = () => {
   );
 };
 
-export { PostContainer, MiniPostContainer };
+const CommentContainer = () => {
+  return (
+    <>
+      <div className="comment-container">
+        <div className="cc01">
+          <div className="cc01-img-container">
+            <img src={process.env.PUBLIC_URL + 'img/cam.jpg'} />
+          </div>
+        </div>
+        <div className="cc02">
+          <h3>catman</h3>
+          <p>2022/02/21</p>
+        </div>
+        <div className="cc03">
+          <FontAwesomeIcon icon={faPen} id="edit-icon" title="수정하기" />
+          <FontAwesomeIcon icon={faX} id="delete-icon" title="삭제하기" />
+        </div>
+        <div className="cc04">
+          <p>
+            와 너무 이뻐요 와 너무 이뻐요 와 너무 이뻐요 와 너무 이뻐요 와 너무
+            이뻐요 와 너무 이뻐요
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { PostContainer, MiniPostContainer, CommentContainer };
