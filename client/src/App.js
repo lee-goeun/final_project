d4abfd294eb561113ba84c71f467ac048275da99
Nam Gideok <namgd1222@gmail.com>
2022-02-21 11:52:27 +0900

    로그인, 회원가입 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,14 @@ import Home from './pages/Home';
 import MyPageLayout from './MyPageLayout';
 import Profile from './pages/Profile';
 import Mypost from './pages/MyPost';
+import Login from './components/authentication/Login';
+import Join from './components/authentication/Join';
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/join" element={<Join />} />
       <Route path="/" element={<Home />} />
       <Route element={<MyPageLayout />}>
         <Route path="/mypost/" element={<Mypost />} />
---
+++ client/src/components/authentication/Join.jsx
@@ -0,0 +1,281 @@
+import { useEffect, useRef, useState } from 'react';
+import './Authentication.css';
+import Footer from '../Footer';
+import { Link } from 'react-router-dom';
+
+const Join = () => {
+  // 아이디, 비밀번호, 비밀번호 재입력 input의 value값 저장
+  const [inputId, setInputId] = useState();
+  const [inputPw, setInputPw] = useState();
+  const [reInputPw, setReInputPw] = useState();
+
+  const idRegex = /^[a-z][a-zA-Z0-9]{5,15}$/; // 아이디 정규표현식
+  const pwRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,15}$/; // 비밀번호 정규표현식
+
+  // 아이디, 비밀번호, 비밀번호 재입력 별 유효성 검사 에러문
+  const [idRegErrorText, setIdRegErrorText] = useState(
+    '영문자(소문자)로 시작하는 6~16자리',
+  );
+  const [pwRegErrorText, setPwRegErrorText] = useState(
+    '숫자, 영문(소·대문자), 특수문자를 포함한 8~16자리',
+  );
+  const [reCheckPwRegErrorText, setReCheckPwRegErrorText] = useState('');
+
+  // 각 유효성 검사 에러문 텍스트 컬러 변경
+  const idRegErrorStyle = useRef();
+  const pwRegErrorStyle = useRef();
+  const reCheckPwRegErrorStyle = useRef();
+
+  // 아이디 유효성 검사
+  useEffect(() => {
+    if (inputId === '' || inputId === undefined) {
+      setIdRegErrorText('영문자(소문자)로 시작하는 6~16자리');
+      idRegErrorStyle.current.style.color = '#494949';
+    } else if (idRegex.test(inputId) === false) {
+      setIdRegErrorText('영문자(소문자)로 시작하는 6~16자리');
+      idRegErrorStyle.current.style.color = 'red';
+    } else if (idRegex.test(inputId) === true) {
+      setIdRegErrorText('올바른 형식의 아이디입니다. 중복을 확인 해주세요.');
+      idRegErrorStyle.current.style.color = '#25d039';
+    }
+  }, [inputId]);
+
+  // 비밀번호 / 비밀번호 재확인 유효성 검사
+  useEffect(() => {
+    if (inputPw === '' || inputPw === undefined) {
+      setPwRegErrorText('숫자, 영문(소·대문자), 특수문자를 포함한 8~16자리');
+      pwRegErrorStyle.current.style.color = '#494949';
+    } else if (pwRegex.test(inputPw) === false) {
+      setPwRegErrorText('숫자, 영문(소·대문자), 특수문자를 포함한 8~16자리');
+      pwRegErrorStyle.current.style.color = 'red';
+    } else if (pwRegex.test(inputPw) === true) {
+      setPwRegErrorText('올바른 형식의 비밀번호 입니다.');
+      pwRegErrorStyle.current.style.color = '#25d039';
+    }
+
+    // 비밀번호 재확인
+    if (reInputPw === '' || reInputPw === undefined) {
+      setReCheckPwRegErrorText('');
+      reCheckPwRegErrorStyle.current.style.color = '#494949';
+    } else if (inputPw !== reInputPw) {
+      setReCheckPwRegErrorText('비밀번호가 일치하지 않습니다.');
+      reCheckPwRegErrorStyle.current.style.color = 'red';
+    } else {
+      setReCheckPwRegErrorText('비밀번호가 일치합니다.');
+      reCheckPwRegErrorStyle.current.style.color = '#25d039';
+    }
+  }, [inputPw, reInputPw]);
+
+  // 약관보기 1창 show/hide
+  const [showTou, setShowTou] = useState(false);
+
+  // 약관 동의 체크 버튼
+  const allTouCheck = useRef();
+  const tou1 = useRef();
+  const tou2 = useRef();
+
+  // 약관 전체 동의 클릭
+  const clickAllTouCheck = (e) => {
+    if (tou1.current.checked === false || tou2.current.checked === false) {
+      tou1.current.checked = true;
+      tou2.current.checked = true;
+    } else if (e.target.checked === false) {
+      tou1.current.checked = false;
+      tou2.current.checked = false;
+    }
+  };
+
+  // 아이디 중복확인 버튼
+  const clickDuplicateCheckBtn = (e) => {
+    alert('아이디 중복확인');
+  };
+
+  const clickSubmitBtn = (e) => {
+    alert('회원가입이 완료되었습니다.');
+  };
+
+  const touModal = useRef();
+
+  return (
+    <>
+      <div className="join-container">
+        <p>아이디</p>
+        <input
+          className="id-input"
+          type="text"
+          name="user_id"
+          placeholder="아이디를 입력하세요"
+          onChange={(e) => {
+            setInputId(e.target.value);
+          }}
+        />
+        <button onClick={clickDuplicateCheckBtn} className="duplicate-check">
+          중복확인
+        </button>
+        <p className="idRegError" ref={idRegErrorStyle}>
+          {idRegErrorText}
+        </p>
+
+        <p>비밀번호</p>
+        <input
+          className="pw-input"
+          type="password"
+          name="user_pw"
+          placeholder="비밀번호를 입력하세요"
+          onChange={(e) => {
+            setInputPw(e.target.value);
+          }}
+        />
+        <p className="pwRegError" ref={pwRegErrorStyle}>
+          {pwRegErrorText}
+        </p>
+
+        <p>비밀번호 재입력</p>
+        <input
+          className="pw-re-input"
+          type="password"
+          placeholder="비밀번호를 재입력하세요"
+          onChange={(e) => {
+            setReInputPw(e.target.value);
+          }}
+        />
+        <p className="pwReCheckRegError" ref={reCheckPwRegErrorStyle}>
+          {reCheckPwRegErrorText}
+        </p>
+
+        <p>이름</p>
+        <input
+          className="name-input"
+          type="text"
+          name="user_name"
+          placeholder="이름을 입력하세요"
+        />
+
+        <p>닉네임</p>
+        <input
+          className="nick-input"
+          type="text"
+          name="user_nick"
+          placeholder="닉네임을 입력하세요"
+        />
+
+        <p>휴대폰 번호</p>
+        <input
+          className="phone-input"
+          type="text"
+          name="user_phone"
+          placeholder="휴대폰 번호를 입력하세요"
+        />
+
+        <p>주소</p>
+        <input
+          className="adr-input"
+          type="text"
+          name="user_addr"
+          placeholder="도로명 주소 찾기"
+        />
+
+        <p>연령대</p>
+        <select className="ages-select" name="user_age">
+          <option value="0">연령대를 선택하세요</option>
+          <option value="10">10대</option>
+          <option value="20">20대</option>
+          <option value="30">30대</option>
+          <option value="40">40대</option>
+          <option value="50">50대</option>
+          <option value="60">60대 이상</option>
+        </select>
+        <br />
+
+        <p>성별</p>
+        <input type="radio" name="user_sex" id="male" value="male" />
+        <label htmlFor="male">남자</label>
+        <input type="radio" name="user_sex" id="female" value="female" />
+        <label htmlFor="female">여자</label>
+
+        <br />
+        <br />
+
+        <div className="show-terms-of-use">
+          <div>
+            <input
+              ref={allTouCheck}
+              onClick={clickAllTouCheck}
+              type="checkbox"
+              value="all-agree"
+              id="all-terms-of-use"
+            />
+            <label htmlFor="all-terms-of-use">약관 전체 동의</label>
+          </div>
+          <p
+            className="show"
+            onClick={() => {
+              setShowTou(!showTou);
+            }}
+          >
+            약관보기
+          </p>
+        </div>
+
+        <div className="terms-of-use-01">
+          <input
+            ref={tou1}
+            type="checkbox"
+            name="location_agree"
+            id="lo-agree"
+          />
+          <label htmlFor="lo-agree">
+            위치기반 서비스 이용약관에 동의합니다.
+          </label>
+        </div>
+
+        <div className="terms-of-use-02">
+          <input
+            ref={tou2}
+            type="checkbox"
+            name="service_ agree"
+            id="svc-agree"
+          />
+          <label htmlFor="svc-agree">서비스 이용약관에 동의합니다.</label>
+        </div>
+
+        <Link to="/login">
+          <button className="back-btn">뒤로가기</button>
+        </Link>
+        <button className="join-btn" onClick={clickSubmitBtn}>
+          회원가입
+        </button>
+      </div>
+
+      {/* 이용 약관 모달창 */}
+      {showTou ? (
+        <div className="tou-modal-wrapper">
+          <div ref={touModal} className="tou-modal-container">
+            <h3>위치기반 서비스 이용약관</h3>
+            <textarea
+              readOnly
+              value="제1조(목적) - 이 약관은 회사가(이하 'P＆P') 제공하는 위치기반서비스에 대해 P＆P와 위치기반서비스를 이용하는 개인위치정보주체(이하 '이용자')간의 권리·의무 및 책임사항, 기타 필요한 사항 규정을 목적으로 합니다."
+            ></textarea>
+            <h3>Pet ＆ Pet 서비스 이용약관</h3>
+            <textarea
+              readOnly
+              value="제1조(목적) - 이 약관은 회사가 온라인으로 제공하는 디지털 콘텐츠 (이하 'P＆P'라고 한다) 및 제반 서비스의 이용과 관련하여 P＆P와 이용자와의 권리, 의무 및 책임사항 등을 규정함을 목적으로 합니다."
+            ></textarea>
+            <br />
+            <button
+              onClick={() => {
+                setShowTou(!showTou);
+              }}
+            >
+              확인
+            </button>
+          </div>
+        </div>
+      ) : null}
+      <Footer />
+    </>
+  );
+};
+
+export default Join;
