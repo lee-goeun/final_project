36c62a167b5d5725d5908269d47e3295db66f77d
padahkim <padahkim@gmail.com>
2022-02-23 00:47:36 +0900

    리덕스 연결중...

---
+++ client/src/redux/modules/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import profileImgButtons from './profileImgButtons';
+
+const rootReducer = combineReducers({
+  profileImgButtons,
+});
+
+export default rootReducer;
