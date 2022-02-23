f4f476e695bde29bceba70e3d74082cf3942002b
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-02-24 04:18:48 +0900

    login/join[js], 쿼리, 라우터, 컨트롤러 정리 및 재작성 주소 데이터값은 아직 미완성입니다. 프론트 변수명으로 우선 맞췄습니다

--- server/controllers/signController.js
+++ server/controllers/signController.js
@@ -1,59 +1,83 @@
-'use strict'
+'use strict';
 
-// const logger = require('../config/logger')
-const User = require('../models/User')
+// 로그 값 찍어줄 곳 위치
+// const logger = require('../db/logger');
+// DB 연동해주는 곳 <- 본인 임시 용
+const User = require('../db/mysql');
 
 const output = {
-  // main page받았을 때 <- render만 임의로 해놓은 것입니다.
-  main: (req, res) => {
-    logger.info(`GET / 304 "홈 화면으로 이동"`)
-    res.render('/main')
+  home: (req, res) => {
+    logger.info(`GET / 304 "홈 화면으로 이동"`);
+    // test용 메인페이지
+    res.render('index');
   },
 
-  // 로그인 render 위치 조정
   login: (req, res) => {
-    logger.info(`GET /login 304 "로그인 화면으로 이동"`)
-    res.render('/login')
+    logger.info(`GET /login 304 "로그인 화면으로 이동"`);
+    // test용 로그인 페이지
+    res.render('login');
   },
 
-  // join 위치조정
-  register: (req, res) => {
-    logger.info(`GET /register 304 "회원가입 화면으로 이동"`)
-    res.render('/join')
+  join: (req, res) => {
+    logger.info(`GET /join 304 "회원가입 화면으로 이동"`);
+    // test용 회원가입 페이지
+    res.render('join');
   },
-}
+};
 
+// 로그인 기능
 const process = {
+  // id, pw 확인
   login: async (req, res) => {
-    const user = new User(req.body)
-    const response = await user.login()
+    const user = new User(req.body);
+    const response = await user.login();
 
     const url = {
       method: 'POST',
       path: '/login',
       status: response.err ? 400 : 200,
-    }
+    };
 
-    log(response, url)
-    return res.status(url.status).json(response)
+    // object 만듦
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 
-  register: async (req, res) => {
-    const user = new User(req.body)
-    const response = await user.register()
+  // 저장
+  join: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.join();
 
+    // 조인 함수
     const url = {
       method: 'POST',
       path: '/join',
       status: response.err ? 409 : 201,
-    }
+    };
 
-    log(response, url)
-    return res.status(url.status).json(response)
+    // 회원가입에 실패한 경우
+    log(response, url);
+    return res.status(url.status).json(response);
   },
-}
+};
 
+// 모듈
 module.exports = {
   output,
   process,
-}
+};
+
+// 로그 찍기
+// const log = (response, url) => {
+//   if (response.err) {
+//     logger.error(
+//       `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.err}`
+//     );
+//   } else {
+//     logger.info(
+//       `${url.method} ${url.path} ${url.status} Response: ${response.success} ${
+//         response.msg || ''
+//       }`
+//     );
+//   }
+// };
