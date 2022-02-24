1f386016baef85457d80e74ce8985e1b60c683f1
서성조 <67176549+seongjo-seo@users.noreply.github.com>
2022-02-24 16:19:08 +0900

    로그인, 회원가입 기능 완료 세션 쿠키로 유지 및 상세 데이터 조정 진행중입니다

--- server/models/UserStorage.js
+++ server/models/UserStorage.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-const db = require('../db/mysql');
+const db = require('../config/mysql')
 
-// 비밀번호 암호화를 위한 선언문 아직 후에 추가될 예정입니다.
-const bcrypt = require('bcrypt-nodejs');
+// 비밀번호 암호화를 위한 선언문
+// const bcrypt = require('bcrypt')
 
 // #는 public에서 private로 변경해 준다.
 // static은 class에 바로 접근을 위해서
@@ -13,23 +13,25 @@ class UserStorage {
     // 프로미스는 수행하는 동작이 끝남과 동시에 상태를 알려준다.
     // 그러므로 비동기 처리에 아주 효과적이다.
     return new Promise((resolve, reject) => {
-      const query = 'SELECT * FROM usertbl WHERE userId = ?;';
+      const query = 'SELECT * FROM usertbl WHERE userId = ?;'
       db.query(query, [userId], (err, data) => {
         // object 형식(?)
-        if (err) reject(`${err}`);
-        else resolve(data[0]);
-      });
-    });
+        if (err) {
+          reject(`${err}`)
+          console.log(data)
+        } else {
+          resolve(data[0])
+        }
+      })
+    })
   }
 
   // id 저장
   static async save(userInfo) {
     return new Promise((resolve, reject) => {
-      // bcrypt.hash( userPw, null, null, function(err, hash)) hash 값 이용해서 저장
-      // 비밀번호 암호화는 후순위로 이후에 하기 [메모]
-
+      // bcrypt.hash( userPw, null, null, function(err, hash))  hash 값 이용해서 저장은 이후에 하기
       const query =
-        'INSERT INTO usertbl(userId, userName, userPw, userNick, userPhone, zonecode, address, detailAddress, extraAddress) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);';
+        'INSERT INTO usertbl(userId, userName, userPw, userNick, userPhone, zonecode, address, detailAddress, extraAddress) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);'
       db.query(
         // db에 날려주는 쿼리문 저장
         query,
@@ -46,12 +48,12 @@ class UserStorage {
         ],
         // err 처리
         (err) => {
-          if (err) reject(`${err}`);
-          else resolve({ success: true });
+          if (err) reject(`${err}`)
+          else resolve({ success: true })
         }
-      );
-    });
+      )
+    })
   }
 }
 
-module.exports = UserStorage;
+module.exports = UserStorage
