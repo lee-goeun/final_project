44faba9c9a67ad0249d208ea74519a3cda1bc44a
lee-goeun <koeun1120@duam.net>
2022-02-25 13:23:22 +0900

    메이트 찾기 ocr 테스트

--- server/routes/Match.js
+++ server/routes/Match.js
@@ -3,11 +3,14 @@ const conn = require('../db/index');
 const router = express.Router();
 const { json } = require('body-parser');
 
+const crypto = require('crypto');
+
 //파일 업로드용 미들웨어
 const multer = require('multer');
 const fs = require('fs');
+const axios  = require('axios');
 
-var storage = multer.diskStorage({
+var matchStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'matchImages/temp'); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
   },
@@ -16,7 +19,17 @@ var storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+var ocrStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'ocrImages'); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // cb 콜백함수를 통해 전송된 파일 이름 설정
+  },
+});
+
+const matchUpload = multer({ storage: matchStorage });
+const ocrUpload = multer({storage:ocrStorage});
 
 //조회(검색)
 router.get('/list', (req, res) => {
@@ -54,12 +67,14 @@ router.get('/listLimit1', (req, res) => {
 });
 
 //추가
-router.post('/add', upload.single('matchImgName'), (req, res) => {
+router.post('/add', matchUpload.single('matchImgName'), (req, res) => {
+  console.log('req', req.file);
+  console.log('req', req.session);
   var body = req.body;
   var filename = req.file.originalname;
 
   var sql =
-    'INSERT INTO matchTbl(user_id, matchImgName, matchTitle, matchContent, selectPet, matchTime) VALUES(?, ?, ?, ?,?, ?);';
+    'INSERT INTO matchTbl(userId, matchImgName, matchTitle, matchContent, selectPet, matchTime) VALUES(?, ?, ?, ?,?, ?);';
   conn.query(
     sql,
     [
@@ -102,9 +117,35 @@ router.post('/add', upload.single('matchImgName'), (req, res) => {
   );
 });
 
-//신분증확인(ocr기능)
-router.post('/confirmId', upload.single('idCard'),(req,res) => {
-  
+//신분확인(ocr기능)
+router.post('/confirmId', ocrUpload.single('idCard'),(req,res) => {
+  console.log('req',req.file);
+  const apiURL = "https://yc2zdolfbc.apigw.ntruss.com/custom/v1/14471/4d0fecd9abbb9baf72dc48c4317fdff307aec28cc83e268e49b6cc94c1cd13de/infer";
+  //const secretKey = ;
+
+
+  const boundary = '----' + crypto.randomUUID().toString().replaceAll('-','');
+  axios.post(apiURL,{
+    headers : {
+      "Content-Type" : "multipart/form-data; boundary=" + boundary,
+      "X-OCR-SECRET" : "aURGRmpuSlJSdFZaanNsU3NmbHBhSmlWTWVOaFNXb3Q="
+    },
+    data: {
+      "version" : "V2",
+      "requestId" : crypto.randomUUID(),
+      "timestamp" : 0,
+      "lang" : "ko",
+      "images": [{
+        "format":"jpg",
+        "url" : req.file.path,
+        "name" : req.file.fieldname
+      }]
+    }
+  }).then(res => {
+    console.log(res.data);
+  }).catch(res => {
+    console.log(res);
+  })
 });
 
 //이미지 읽어오는 경로
@@ -130,7 +171,7 @@ router.get('/detail/:id', (req, res) => {
 });
 
 //수정
-router.put('/mod', upload.single('matchImgName'), (req, res) => {
+router.put('/mod', matchUpload.single('matchImgName'), (req, res) => {
     var body = req.body;
     var image = req.file.originalname;
     console.log("req", body, req.file);
@@ -138,18 +179,18 @@ router.put('/mod', upload.single('matchImgName'), (req, res) => {
     conn.query(sql, [image, body.matchTitle, body.matchContent, body.selectPet, body.matchTime, body.matchId],(err, results) => {
         if(err) return res.json({success:false, err});
         else {
-                var newdir = "matchImages/" + body.matchId + "/";
+          var newdir = "matchImages/" + body.matchId + "/";
 
-        if (!fs.existsSync(newdir)) {
-          fs.mkdirSync(newdir);
-        }
+          if (!fs.existsSync(newdir)) {
+            fs.mkdirSync(newdir);
+          }
 
-        var oldPath = 'matchImages/temp/' + image;
-        var newPath = newdir + image;
+          var oldPath = 'matchImages/temp/' + image;
+          var newPath = newdir + image;
 
-        fs.rename(oldPath, newPath, function (err) {
-          if (err) throw err;
-          console.log('move success');
+          fs.rename(oldPath, newPath, function (err) {
+            if (err) throw err;
+            console.log('move success');
         });
 
         //TODO: 이전 이미지 삭제
