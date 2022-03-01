c5946bd965baf34a8f5022395e90dfb4199ff569
Nam Gideok <namgd1222@gmail.com>
2022-03-01 16:50:31 +0900

    채팅유저컨테이너 props 추가

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,66 +3,32 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { PostContainer } from '../components/Post';
 import styled from 'styled-components';
+import Slider from 'react-slick';
 
 const MainPageFooterStyle = styled.div`
   .main-body-div {
     height: 1077px;
     padding: 38px 0;
   }
-  .btn-wrapper {
-    display: flex;
-    flex-direction: row;
-  }
-  .left-btn-div,
-  .right-btn-div {
-    position: relative;
-    bottom: 630px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    width: fit-content;
-    transition: 0.3s;
-  }
-  .left-btn-div {
-    right: 10%;
-  }
-  .right-btn-div {
-    left: 10%;
-  }
-
-  @media screen and (max-width: 2250px) {
-    .left-btn-div {
-      right: 16%;
-    }
-    .right-btn-div {
-      left: 16%;
-    }
-  }
-  @media screen and (max-width: 2000px) {
-    .left-btn-div {
-      width: 150px;
-      height: 150px;
-      bottom: 590px;
-    }
-    .right-btn-div {
-      width: 150px;
-      height: 150px;
-      bottom: 590px;
-    }
-  }
 `;
 
 const Home = () => {
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 500,
+  };
+
   return (
     <>
       <Header />
       <MainPageFooterStyle>
         <div className="main-body-div">
           <PostContainer />
-          <div className="btn-wrapper">
-            <div className="left-btn-div"></div>
-            <div className="right-btn-div"></div>
-          </div>
+          <Slider {...settings}></Slider>
         </div>
       </MainPageFooterStyle>
       <Footer />
