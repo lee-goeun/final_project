cc99c2d9450f2dcd0dd0e65c094d4d3e6d0ff22e
SongJiYoung <songjjy0108@kookmin.ac.kr>
2022-03-21 11:32:46 +0900

    App.js [] 제거

--- client/src/App.js
+++ client/src/App.js
@@ -56,7 +56,7 @@ const App = () => {
   useEffect(() => {
     getAuth();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); //
+  }); //
 
   const getAuth = async () => {
     try {
