8323c209083579dac39e8aa7011bdb6f6d39ed84
Nam Gideok <namgd1222@gmail.com>
2022-03-21 15:47:24 +0900

    프로필 관리 페이지 - 하단 여백 추가

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,8 +7,16 @@ import UserAvatar from '../components/common/UserAvatar';
 import axios from 'axios';
 import ProfileUpdateModal from '../components/profile/ProfileUpdateModal';
 
+import styled from 'styled-components';
+
+const BoxStyle = styled.div`
+  #boxboxbox {
+    margin: 5px auto 100px auto;
+  }
+`;
+
 const Profile = ({ userInfoProps }) => {
-  console.log('userInfoProps',userInfoProps);
+  console.log('userInfoProps', userInfoProps);
   const {
     userId,
     userName,
@@ -52,115 +60,117 @@ const Profile = ({ userInfoProps }) => {
 
   // 급하게 만든 디자인 페이지 디테일한 부분은 필요합니다
   return (
-    <div className={styles.register}>
-      <h3>프로필</h3>
-      <form onSubmit={register}>
-        <div className={styles.flex}>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>개인 프로필</li>
-            <li className={styles.item}>
-              <UserAvatar sx={{ height: '70px', width: '70px' }} />
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>아이디</li>
-            <li className={styles.item}>
-              <text>{userId}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>이름</li>
-            <li className={styles.item}>
-              <text>{userName}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>닉네임</li>
-            <li className={styles.item}>
-              <text>{userNick}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>잔액</li>
-            <li className={styles.item}>
-              <text>{balance}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>이메일</li>
-            <li className={styles.item}>
-              <text>{userEmail}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>전화번호</li>
-            <li className={styles.item}>
-              <text>{userPhone}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>우편번호</li>
-            <li className={styles.item}>
-              <text>{zonecode}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>주소</li>
-            <li className={styles.item}>
-              <text>{address}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>상세주소</li>
-            <li className={styles.item}>
-              <text>{detailAddress}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}>소개</li>
-            <li className={styles.item}>
-              <text>{info}</text>
-            </li>
-            <li className={styles.item}></li>
-          </ul>
-          <ul className={styles.container}>
-            <li className={`${styles.item} ${styles.center}`}></li>
-            <li className={styles.item}></li>
-            <li className={styles.item}>
-              <Button
-                type="button"
-                onClick={() => {
-                  setShowUpdateModal(true);
-                }}
-              >
-                수정하기
-              </Button>
-              <Button onClick={clickDeleteBtn} name="submitChangeInfo">
-                회원탈퇴
-              </Button>
-            </li>
-          </ul>
-        </div>
-      </form>
-      {showUpdateModal ? (
-        <ProfileUpdateModal
-          clickCancel={() => {
-            setShowUpdateModal(false);
-          }}
-          userInfoProps={userInfoProps}
-        />
-      ) : null}
-    </div>
+    <BoxStyle>
+      <div id="boxboxbox" className={styles.register}>
+        <h3>프로필</h3>
+        <form onSubmit={register}>
+          <div className={styles.flex}>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>개인 프로필</li>
+              <li className={styles.item}>
+                <UserAvatar sx={{ height: '70px', width: '70px' }} />
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>아이디</li>
+              <li className={styles.item}>
+                <text>{userId}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>이름</li>
+              <li className={styles.item}>
+                <text>{userName}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>닉네임</li>
+              <li className={styles.item}>
+                <text>{userNick}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>잔액</li>
+              <li className={styles.item}>
+                <text>{balance}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>이메일</li>
+              <li className={styles.item}>
+                <text>{userEmail}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>전화번호</li>
+              <li className={styles.item}>
+                <text>{userPhone}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>우편번호</li>
+              <li className={styles.item}>
+                <text>{zonecode}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>주소</li>
+              <li className={styles.item}>
+                <text>{address}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>상세주소</li>
+              <li className={styles.item}>
+                <text>{detailAddress}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}>소개</li>
+              <li className={styles.item}>
+                <text>{info}</text>
+              </li>
+              <li className={styles.item}></li>
+            </ul>
+            <ul className={styles.container}>
+              <li className={`${styles.item} ${styles.center}`}></li>
+              <li className={styles.item}></li>
+              <li className={styles.item}>
+                <Button
+                  type="button"
+                  onClick={() => {
+                    setShowUpdateModal(true);
+                  }}
+                >
+                  수정하기
+                </Button>
+                <Button onClick={clickDeleteBtn} name="submitChangeInfo">
+                  회원탈퇴
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </form>
+        {showUpdateModal ? (
+          <ProfileUpdateModal
+            clickCancel={() => {
+              setShowUpdateModal(false);
+            }}
+            userInfoProps={userInfoProps}
+          />
+        ) : null}
+      </div>
+    </BoxStyle>
   );
 };
 
